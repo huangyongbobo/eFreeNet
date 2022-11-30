e890e7db50e0b825a9673f06c51b88ea12d3c08c
huangyongbo <66212098+huangyongbobo@users.noreply.github.com>
2022-11-30 14:56:55 +0800

    Update RSOC_building_preprocess.py

--- Dataset/RSOC_building_preprocess.py
+++ Dataset/RSOC_building_preprocess.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 
 def main():
     # set the root to the building dataset
-    root = '/media/ysliu/6b94d4ca-f5c4-46ae-8497-af46d2544dfc/Maoer/RSOC_building'
+    root = '.../RSOC_building'
     building_train = os.path.join(root, 'train_data', 'images')  # path of train image
     building_test = os.path.join(root, 'test_data', 'images')  # path of test image
 
